50fd2f6caad792bc661073e6689b79f749939ff5
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-01 16:19:34 -0700

    adds spec testing for subscription validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,8 @@ class Subscription < ApplicationRecord
   has_many :tea_subscriptions
   has_many :teas, through: :tea_subscriptions
 
+  
+
   enum status: { active: 0, cancelled: 1 }
   enum frequency: { '1_month' => 0, '2_month' => 1, '3_month' => 2 }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Subscription, type: :model do
     it { should have_many :tea_subscriptions }
     it { should have_many(:teas).through(:tea_subscriptions) }
   end
+
+  describe 'validations' do
+    it { should validate presence_of :title }
+    it { should validate presence_of :price }
+    it { should validate presence_of :status }
+    it { should validate presence_of :frequency }
+    it { should validate presence_of :customer_id }
+  end
 end
