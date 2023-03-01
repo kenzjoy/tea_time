fa0ccfafe38cb681d87f4b0174c40d02445e0189
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-01 16:14:20 -0700

    adds default value to subscription status and frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,6 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   has_many :tea_subscriptions
   has_many :teas, through: :tea_subscriptions
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_01_225858) do
+ActiveRecord::Schema.define(version: 2023_03_01_230759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2023_03_01_225858) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.float "price"
-    t.integer "status"
-    t.integer "frequency"
+    t.integer "status", default: 0
+    t.integer "frequency", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
