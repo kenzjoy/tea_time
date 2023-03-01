fc27e80b7bf63bd99596f0b85010a6d022b0c428
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-01 16:29:19 -0700

    validates tea attributes

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,9 @@
 class Tea < ApplicationRecord
   has_many :tea_subscriptions
   has_many :subscriptions, through: :tea_subscriptions
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :temperature
+  validates_presence_of :brew_time
 end
