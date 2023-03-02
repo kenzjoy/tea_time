4c4c8593be8c0aff2be3278d7d15ee62715f49f9
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-02 10:07:45 -0700

    sad path for invalid customer id

--- spec/requests/api/v1/customer_subscriptions/create_spec.rb
+++ spec/requests/api/v1/customer_subscriptions/create_spec.rb
@@ -49,4 +49,42 @@ RSpec.describe 'POST /customer-subscriptions' do
       expect(kenz.subscriptions.count).to eq(1)
     end
   end
+
+  describe 'when a customer_id is not valid' do
+    it 'returns an error message' do
+      tea_1 = Tea.create!(
+        id: 5,
+        title: 'Orange Clove Tea',
+        description: 'Support your overall health with Yogi Orange Clove tea, a limited-edition blend sold during the holidays. Bright Orange Peel and aromatic Clove combine with Astragalus, a herb traditionally used for its ability to help support immune function. Traditional Ayurvedic warming spices including Cinnamon, Cardamom, Ginger, and Black Pepper add delicious spiced notes. The result is a perfectly spiced and citrusy herbal tea blend that is sure to make your holidays merry and bright.',
+        temperature: 'boiling water',
+        brew_time: '7 minutes',
+        image: 'https://yogiproducts.com/wp-content/uploads/2019/10/YT-US-OrangeClove-CAR-C23-202446-3DFront-WithGlow-300DPI-PNG.png'
+      )
+      subscription_1 = Subscription.create!(
+        id: 1,
+        tea_id: 5,
+        title: 'Orange Clove Tea',
+        price: 7.99,
+        status: 0,
+        frequency: 0
+      )
+
+      headers = { "Content-Type": "application/json", Accept: "application/json" }
+      body = ( {
+        "customer_id": nil,
+        "subscription_id": 1
+      } )
+
+      post '/api/v1/customer-subscriptions', headers: headers, params: JSON.generate(body)
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed).to be_a(Hash)
+      expect(parsed).to have_key(:error)
+      expect(parsed[:error]).to eq('Subscription Not Added')
+    end
+  end
 end
