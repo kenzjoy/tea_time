11fb5dcd5e48fce1e0ff15901f8745e9db167000
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-02 10:14:50 -0700

    adds edge case for avoiding duplicate subscriptions

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
   def create
      customer_subscription = CustomerSubscription.new(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
      if customer_subscription.save
-      render json: { 'success': 'Subscription Added'}
+      render json: { 'success': 'Subscription Added' }, status: 200
      else
-      render json: { 'error': 'Subscription Not Added'}
+      render json: { 'error': 'Subscription Not Added' }, status: 200
      end
   end
 end
