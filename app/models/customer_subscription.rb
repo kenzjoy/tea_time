119fc43c998b5e092a62af64001e37d78bdfa89d
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-02 12:45:13 -0700

    creates edge case to keep a user from having multiple of the same subscription

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -2,6 +2,5 @@ class CustomerSubscription < ApplicationRecord
   belongs_to :customer
   belongs_to :subscription
 
-  validates_presence_of :customer_id
-  validates_presence_of :subscription_id
+  validates :customer_id, uniqueness: { scope: :subscription_id }
 end
--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe CustomerSubscription, type: :model do
     it { should belong_to :customer }
     it { should belong_to :subscription }
   end
-
-  describe 'validations' do
-    it { should validate_presence_of :customer_id }
-    it { should validate_presence_of :subscription_id }
-  end
 end
