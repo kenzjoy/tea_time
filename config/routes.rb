043d619ee60596e2467815ed14fd87de7d1259b1
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-02 11:58:59 -0700

    happy path test for customer show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get '/customer', to: 'customers#show'
       post '/customer-subscriptions', to: 'customer_subscriptions#create'
     end
   end
---
+++ spec/requests/api/v1/customers/show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'GET /customer' do
+  describe 'when the records exist' do
+    it 'returns a specified user and their favorite trails in json' do
+      kenz = Customer.create!(
+        id: 1,
+        first_name: 'Kenz',
+        last_name: 'Leng',
+        email: 'kenz@ilovetea.com',
+        address: '318 E 3rd Ave Durango, CO 81301'
+      )
+
+      get '/api/v1/customer?id=1'
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      parsed = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed).to be_a(Hash)
+      expect(parsed).to have_key(:data)
+      expect(parsed[:data]).to be_a(Hash)
+
+      expect(parsed[:data]).to have_key(:id)
+      expect(parsed[:data][:id]).to be_a(String)
+      expect(parsed[:data]).to have_key(:type)
+      expect(parsed[:data][:type]).to be_a(String)
+      expect(parsed[:data]).to have_key(:attributes)
+      expect(parsed[:data][:attributes]).to be_a(Hash)
+      expect(parsed[:data][:attributes]).to have_key(:first_name)
+      expect(parsed[:data][:attributes][:first_name]).to be_a(String)
+      expect(parsed[:data][:attributes]).to have_key(:last_name)
+      expect(parsed[:data][:attributes][:last_name]).to be_a(String)
+      expect(parsed[:data][:attributes]).to have_key(:email)
+      expect(parsed[:data][:attributes][:email]).to be_a(String)
+      expect(parsed[:data][:attributes]).to have_key(:address)
+      expect(parsed[:data][:attributes][:address]).to be_a(String)
+      expect(parsed[:data][:attributes]).to have_key(:subscriptions)
+      expect(parsed[:data][:attributes][:subscriptions]).to be_an(Array)
+    end
+  end
+end
